f5c384a8519a4efc09d7c1b13f8397d0da083c35
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-29 22:13:54 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+
+class Person
+ 
+  def initialize(his_name, his_job)
+    @name = his_name
+  end
+ 
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+ 
+end
